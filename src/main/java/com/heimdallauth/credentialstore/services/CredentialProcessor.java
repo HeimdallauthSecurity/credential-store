31667f242151a2af5824cff9c73b7179afc5d3bb
Mayank Soni <106593569+mayanksoni1996@users.noreply.github.com>
2024-09-06 19:18:42 +0530

    improve(CredentialValidation): HCS-2 HCS-14 Update Credential processor to process validation of password credentials

--- src/main/java/com/heimdallauth/credentialstore/services/CredentialProcessor.java
+++ src/main/java/com/heimdallauth/credentialstore/services/CredentialProcessor.java
@@ -1,8 +1,16 @@
 package com.heimdallauth.credentialstore.services;
 
+import com.heimdallauth.credentialstore.constants.CredentialType;
 import com.heimdallauth.credentialstore.dao.CredentialsDataManager;
+import com.heimdallauth.credentialstore.dao.PasswordCredentialDocument;
+import com.heimdallauth.credentialstore.dto.CredentialValidationResponse;
+import com.heimdallauth.credentialstore.exceptions.CredentialNotFoundInDB;
+import com.heimdallauth.credentialstore.exceptions.CredentialValidationFailed;
+import com.heimdallauth.credentialstore.exceptions.CredentialsAreExpired;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
 
 @Service
 public class CredentialProcessor {
@@ -19,5 +27,26 @@ public class CredentialProcessor {
         String hashedPassword = this.vaultServices.hashPassword(decryptedTransitPassword);
         String encryptedPassword = this.vaultServices.encryptUsingCredentialStoreKey(hashedPassword);
         this.credentialsDataManager.savePasswordCredentialDocument(profileId, tenantId, encryptedPassword);
+    }
+
+    public String validateUserPasswordCredential(String profileId, String transitEncryptedPassword) {
+        String decryptedTransitPassword = this.vaultServices.decryptFromTransit(transitEncryptedPassword);
+        PasswordCredentialDocument passwordCredentialDocument = this.credentialsDataManager.findPasswordCredentialByProfileResourceNumber(profileId).orElseThrow(() -> new CredentialNotFoundInDB("Requested credentials could not be found in DB"));
+        if (passwordCredentialDocument.getExpiresOn().isBefore(Instant.now())) {
+            // Credential is still valid
+            Boolean isCredentialValidated = this.vaultServices.isPasswordValid(passwordCredentialDocument.getCipherText(), decryptedTransitPassword);
+            if (isCredentialValidated) {
+                CredentialValidationResponse response = new CredentialValidationResponse(
+                        true,
+                        profileId,
+                        "",
+                        CredentialType.PASSWORD_CREDENTIAL.toString()
+                );
+                return this.vaultServices.encryptUsingCommonKey(response);
+            }
+        } else {
+            throw new CredentialValidationFailed("Credential validation failed");
+        }
+        throw new CredentialsAreExpired("Credentials are expired", profileId);
     }
 }
